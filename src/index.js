b281ddc5f7e477389cd79fc869473ccc1f28b36d
sabra akhavan <sabraxavan@gmail.com>
2022-03-02 16:15:16 +0330

    [ADD]: added flippablecard

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap-grid.rtl.min.css';
-import 'bootstrap/dist/css/bootstrap-utilities.rtl.min.css';
-import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+import "./Normalize.css";
+import "bootstrap/dist/css/bootstrap-grid.rtl.min.css";
+import "bootstrap/dist/css/bootstrap-utilities.rtl.min.css";
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
