4f48e4edee24d3c5405833192935f483d11f6621
sabra akhavan <sabraxavan@gmail.com>
2022-03-08 16:54:45 +0330

    sdfghjk

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import VilaFinder from "./components/VilaFinder";
 import FlippableCard from "./components/FlippableCard/FlippableCard";
 import "./App.scss";
 import TopNavImage from "./assets/image/e556e92c-a57b-4f11-8eea-2b679d377b3e.jpg";
-import Logo from "./assets/image/logo.png"
+import Logo from "./assets/image/logo.png";
 import CustomButton from "./components/CustomButton/CustomButton";
 import TopNavBanner from "./components/TopNavBanner/TopNavBanner";
 import Header from "./components/Header/Header";
 import Heading from "./components/Heading/Heading";
-import Footer from './components/Footer/Footer'
+import Footer from "./components/Footer/Footer";
 function App() {
   const cardData = [
     {
@@ -26,7 +26,7 @@ function App() {
         "https://cdn.jabama.com/image/jabama-images/1447261/557a9a44-c06b-4b7a-8d18-23295f6a33b3.png",
     },
     {
-      id:2,
+      id: 2,
       frontIcon: "fa fa-home",
       frontTitle: "front title 2",
       frontDesc: "front desc",
@@ -35,7 +35,7 @@ function App() {
       status: "top",
       backgroundImageOverlay: "rgba(0, 0, 0, 0.44)",
       backDesc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+        "Lorem Ipsum is dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
       backgroundImage:
         "https://cdn.jabama.com/image/jabama-images/1447261/034720d1-4929-4c0f-a151-1c1e417f4c5e.png",
     },
