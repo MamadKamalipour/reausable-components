ad48db6ffd241c1e6769c3814bc91d32aa1cb37b
Mohammad kamalipour <mohammad.kp8093@gmail.com>
2022-04-10 12:46:18 +0430

    mamads changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./Normalize.css";
+import "react-multi-carousel/lib/styles.css";
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
 import "bootstrap/dist/css/bootstrap-grid.rtl.min.css";
 import "bootstrap/dist/css/bootstrap-utilities.rtl.min.css";
 import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import "react-multi-carousel/lib/styles.css";
-import "react-dates/initialize";
-import "react-dates/lib/css/_datepicker.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
